7a98b0505a5e98099083036ce27f59edb9e91e3b
jerome.pericart <jerome.pericart@809bb202-0a34-0410-b1e5-bbb5a30e7654>
2007-06-29 18:23:19 +0000

    Creation des premières classes + ajouts des drapeaux

--- trunk/rael/src/net/mauhiz/rael/Artiste.java
+++ trunk/rael/src/net/mauhiz/rael/Artiste.java
@@ -1,5 +1,12 @@
 package net.mauhiz.rael;
-
-public class Artiste {
-
+public class Artiste implements Comparable<Artiste> {
+    private Biographie bio;
+    private String     nom;
+    private Pays       pays_dorigine;
+    /**
+     * Simple Classement alphabétique
+     */
+    public int compareTo(final Artiste o) {
+        return this.nom.compareTo(o.nom);
+    }
 }
