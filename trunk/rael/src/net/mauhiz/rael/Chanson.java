48283c522722330722ad3c787c6c7d19e990b723
jerome.pericart <jerome.pericart@809bb202-0a34-0410-b1e5-bbb5a30e7654>
2007-07-01 10:27:01 +0000

    ajout de la classe Chanson que j'ai oublié de commit

---
+++ trunk/rael/src/net/mauhiz/rael/Chanson.java
@@ -0,0 +1,21 @@
+package net.mauhiz.rael;
+
+/**
+ * Représente une chanson, indépendante d'un album ou d'un groupe
+ * 
+ * @author All Users
+ */
+public class Chanson {
+    /**
+     * Compositeur de la chanson
+     */
+    private String compositeur;
+    /**
+     * Paroles de la chanson
+     */
+    private String paroles;
+    /**
+     * Titre de la chanson
+     */
+    private String titreChanson;
+}
